66a9aba5ec3b0b1643534407603dd0ff1d3b8816
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-06-02 15:20:29 -0400

    intergation parts 2

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+    # """
+    # Render the 'frontend/index.html' template for the given request.
+    # Args:
+    #     request (HttpRequest): The HTTP request object.
+    #     *args: Variable length argument list.
+    #     **kwargs: Arbitrary keyword arguments.
+    # Returns:
+    #     HttpResponse: The rendered HTML response.
+    # """
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
